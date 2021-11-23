f6d2f99a28a191f299fdf11f722fd4fb6e9d133f
tangrenyue <724562793@qq.com>
2021-11-23 23:49:29 +0800

    deepClone

--- js-practice/深浅拷贝.js
+++ js-practice/深浅拷贝.js
@@ -14,6 +14,25 @@ function clone(target) {
   return target
 }
 
+// 深拷贝
+function deepClone(target) {
+  //检测数据的类型
+  if (typeof target === 'object' && target !== null) {
+      const result = Array.isArray(target) ? [] : {};
+      //遍历对象
+      for (let key in target) {
+          //检测该属性是否为对象本身的属性
+          if (target.hasOwnProperty(key)) {
+              //拷贝
+              result[key] = deepClone(target[key]);
+          }
+      }
+      return result;
+  } else { 
+      return target;
+  }
+}
+
 const obj1 = {
   name: '张三',
   age: 18,
@@ -22,10 +41,11 @@ const obj1 = {
     name: '金毛',
     age: 2
   },
-  friends: ['李四', '王五']
+  friends: ['李四', '王五'],
+  test: function(){}
 }
 
-const obj2 = clone(obj1)
+const obj2 = deepClone(obj1)
 // 修改obj1中的成员 会影响obj2
 obj1.dog.name = 'xxx';
 obj1.friends[0] = 'xxx';
